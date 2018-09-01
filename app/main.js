0a723fcad32933e63668585dddc56f6d24b82cea
Ayush Goyal <ayush987goyal@gmail.com>
2018-09-01 11:39:14 +0530

    Adding messages and photo modal

--- app/main.js
+++ app/main.js
@@ -33,8 +33,8 @@ const _init = () => {
       return;
     }
 
-    console.log('adding message');
-    console.log(caption);
+    // Render new message in feed
+    renderMessage({ photo: camera.photo, caption });
 
     // Reset caption field on success
     $('#caption').val('');
@@ -43,4 +43,29 @@ const _init = () => {
       .removeClass('withphoto');
     camera.photo = null;
   });
+};
+
+const renderMessage = message => {
+  let msgHTML = `
+        <div style="display: none;" class="row message bg-light mb-2 rounded shadow">
+            <div class="col-2 p-1">
+                <img src="${message.photo}" class="photo w-100 rounded">
+            </div>
+            <div class="col-10 p-1">${message.caption}</div>
+        </div>
+    `;
+
+  $(msgHTML)
+    .prependTo('#messages')
+    .show(500)
+    // Bind a click handler on img to show modal
+    .find('img')
+    .on('click', showPhoto);
+};
+
+const showPhoto = e => {
+  let photoSrc = $(e.currentTarget).attr('src');
+
+  $('#photoframe img').attr('src', photoSrc);
+  $('#photoframe').modal('show');
 };
