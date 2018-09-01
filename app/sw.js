b37ff0b1f2e44641e1630f8f65a823d205e230f3
Ayush Goyal <ayush987goyal@gmail.com>
2018-09-01 12:40:18 +0530

    Add manual build process woth workbox and manifest

--- app/sw.js
+++ app/sw.js
@@ -1,8 +1,46 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-// Allow new SW to take over
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-// Cache .js files - cache first
-workbox.routing.registerRoute(/\.js$/, workbox.strategies.networkFirst());
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "d2106197e9c4880906553c084078336a"
+  },
+  {
+    "url": "main.js",
+    "revision": "a3395006f71ab5464a3a082dc20c60fe"
+  },
+  {
+    "url": "Classes/Camera.js",
+    "revision": "6a7af21dd527b1b9673a124e74b8e6a4"
+  },
+  {
+    "url": "Classes/Message.js",
+    "revision": "738cf2cf246e9fa16a7e07122102b4f0"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(css|js)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https:\/\/use\.fontawesome\.com.*/, workbox.strategies.staleWhileRevalidate({ cacheName: "fontawesome", plugins: [] }), 'GET');
---
+++ sw_build.js
@@ -0,0 +1,28 @@
+const { generateSW } = require('workbox-build');
+
+generateSW({
+  swDest: 'app/sw.js',
+  globDirectory: 'app',
+  globPatterns: ['**/*.{html,css}', 'main.js', 'Classes/*.js'],
+  skipWaiting: true,
+  clientsClaim: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(css|js)/,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: /^https:\/\/use\.fontawesome\.com.*/,
+      handler: 'staleWhileRevalidate',
+      options: {
+        cacheName: 'fontawesome'
+      }
+    }
+  ]
+})
+  .then(({ count, size }) => {
+    console.log(
+      `Generated new service worker with ${count} precacached files, totaling ${size} bytes.`
+    );
+  })
+  .catch(console.error);
